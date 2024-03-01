2c1b34b97a881a2d81ba62ab3bd4dee14f879f34
elimendo <emendoza8361@gmail.com>
2024-02-29 16:48:06 -0800

    improvements

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
-const cityForm = document.getElementById("cityForm")
+const cityForm = document.getElementById("cityForm");
+const api = "05078ce19f85a3fe9b4a8bf548f7fff7";
 cityForm.addEventListener("submit", function (event) {
-    event.preventDefault()
-    const cityValue =cityForm.querySelector("input").value
-    console.log(cityValue)
-})
+  event.preventDefault();
+  const cityValue = cityForm.querySelector("input").value;
+  getCoordinates(cityValue);
+});
+function getCoordinates(cityValue) {
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&appid=${api}`;
+  fetch(url)
+    .then((response) => {
+        
+      return response.json();
+    })
+    .then((data) => {
+
+      console.log(data);
+      const {lat,lon} = data[0]
+        getWeather(lat, lon)
+    });
+}
+function getWeather (lat,lon) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=imperial`
+    fetch(url)
+    .then((response) => {
+        
+      return response.json();
+    })
+    .then((data) => {
+
+      console.log(data);
+    fiveDay(lat, lon)
+    renderCurrentWeather(data)
+    });
+}
+
+
+    
+function fiveDay (lat, lon){
+  const url =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api}`
+  fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data)=> {
+    
+    console.log(data);
+   
+
+  });
+}
+
+function renderCurrentWeather(data){
+  const parentDiv = document.querySelector('.current')
+  parentDiv.innerHTML=""
+  const clone= document.getElementById("currentWeatherData").content.cloneNode(true)
+  console.log(clone)
+  parentDiv.append(clone)
+}
